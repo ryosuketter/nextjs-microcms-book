92d830bc8f81353da18385c04681ffddc63541cd
ryosuketter <r.shimizu.yale@gmail.com>
2024-11-02 17:35:39 +0900

    feat: until 4-2-2

---
+++ app/members/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+// import styles from './page.module.css';
+
+export default async function Page() {
+  return (
+    <div style={{margin: 20}}>
+      <h1>メンバーページ</h1>
+    </div>
+  );
+}
